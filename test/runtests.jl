240b76971c359e23c80117ee3002f7b1e76c8278
Murat Koptur <mkoptur3@gmail.com>
2019-08-14 10:38:07 +0300

    more tests added

--- test/runtests.jl
+++ test/runtests.jl
@@ -53,6 +53,21 @@ using Test
         )
         gc_content = Bioinformatics.gc_content(seq)
         @test round(gc_content, digits = 2) == 0.61
+
+        seq = Bioinformatics.Sequence(
+            collect(values(Bioinformatics.readFASTA("example_data/P35858.fasta")))[1],
+            "AA"
+        )
+        seq_stats = Bioinformatics.protparam(seq)
+        @test seq_stats["Number of amino acids"] == 605
+        @test seq_stats["Molecular weight"] - 66035.0 < 0.01
+        @test Bioinformatics.isoelectric_point(seq) - 6.35 < 0.01
+        @test seq_stats["# of negatively charged residues"] == 59
+        @test seq_stats["# of positively charged residues"] == 54
+        @test seq_stats["Extinction coefficient"] == 61555
+        @test seq_stats["Instability index"] - 46.09 < 0.01
+        @test seq_stats["Aliphatic index"] - 111.652 < 0.01
+        @test seq_stats["Grand average of hydropathicity (GRAVY)"] - 0.018843 < 0.01
     end
 
 end
